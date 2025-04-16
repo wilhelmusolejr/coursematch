3c2c3772359f1937c53d33003f5a9dc9c6168caa
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-04-16 17:03:25 +0800

    Center emoji

--- app/components/IconWithBg.js
+++ app/components/IconWithBg.js
@@ -2,8 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function IconWithBg({ icon }) {
   return (
-    <div className="w-12 h-12 rounded-md bg-gray-200 p-3 secondary-text-color">
-      <FontAwesomeIcon icon={icon} />
+    <div className="w-12 h-12 rounded-md bg-gray-200 p-3 secondary-text-color flex items-center justify-center">
+      <FontAwesomeIcon icon={icon} className="w-7 h-7" />
     </div>
   );
 }
