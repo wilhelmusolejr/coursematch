49333e22abd923fcc2c8916ba18b23d8df70cdbe
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-09-22 19:09:29 +0800

    Initial commit with client and server

--- app/components/CourseCard.js
+++
@@ -1,5 +0,0 @@
-export default function CourseCard({ children, className = "" }) {
-  return (
-    <div className={`max-w-72 p-5 rounded-xl ${className}`}>{children}</div>
-  );
-}
--- app/components/Label.js
+++
@@ -1,7 +0,0 @@
-export default function Label({ htmlFor, children, required = false }) {
-  return (
-    <label htmlFor={htmlFor} className="capitalize">
-      {children} {required && <span className="text-red-500">*</span>}
-    </label>
-  );
-}
