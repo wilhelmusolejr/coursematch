17bc2120ffc5da1fa392163d417da1c5f624f744
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-09-25 17:20:22 +0800

    feat: add SectionHeading component

---
+++ client/src/components/SectionHeading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SectionHeading({ tagline, heading }) {
+  return (
+    <div className="capitalize mb-20 flex justify-center items-center flex-col gap-5">
+      <div className="rounded-3xl bg-red-200 text-red-800 font-medium  px-8 py-3 w-fit">
+        {tagline}
+      </div>
+      <h2 className="font-bold text-3xl text-center text-secondary">
+        {heading}
+      </h2>
+    </div>
+  );
+}
