23592c42c58d496faf232fb8388e7ae9d66912dd
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-10-01 18:27:44 +0800

    feat: courseprediction component

--- client/src/components/SectionHeading.jsx
+++ client/src/components/SectionHeading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SectionHeading({ tagline, heading }) {
   return (
     <div className="capitalize mb-20 flex justify-center items-center flex-col gap-5">
-      <div className="rounded-3xl bg-red-200 text-red-800 font-medium  px-8 py-3 w-fit">
+      <div className="rounded-3xl bg-red-200 text-red-800 font-medium px-8 py-3 w-fit">
         {tagline}
       </div>
       <h2 className="font-bold text-3xl text-center text-secondary">
