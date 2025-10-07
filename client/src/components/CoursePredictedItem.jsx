86ff5fd7bcbea56796a4a0ed94c226a2687564c0
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-10-07 20:46:59 +0800

    feat: add new element in the card showing model number.

--- client/src/components/CoursePredictedItem.jsx
+++ client/src/components/CoursePredictedItem.jsx
@@ -9,85 +9,96 @@ export default function CoursePredictedItem({
   imgUrl = "",
   college_type = "",
 }) {
-  let classname, heading, tagline;
+  let classname, heading, tagline, model;
 
   switch (college_type) {
     case "aligned":
       classname = "bg-slate-200/50 order-1";
       heading = "Best fit with your strand";
       tagline = "Aligned Choice";
+      model = "1";
       break;
     case "not_aligned":
       classname = "bg-slate-200/40 self-center lg:mt-10 order-2";
       heading = "Alternative outside your strand";
       tagline = "Alternative Path";
+      model = "2";
       break;
     case "mixed":
       classname = "bg-slate-200/30 self-end lg:mt-20 order-3";
       heading = "General top pick";
       tagline = "Explore Everything";
+      model = "3";
       break;
   }
 
   return (
     <div
-      className={`px-5 py-16 rounded-lg border-1 border-black/10  max-w-100 xl:flex-1 ${classname}`}
+      className={`rounded-lg border-1 border-black/10  max-w-100 xl:flex-1 ${classname}`}
     >
-      {/* heading */}
-      <div className="flex justify-center items-center gap-5 flex-col text-center">
-        {/* tagline */}
-        <div className="capitalize rounded-3xl bg-red-200 text-red-800 font-medium px-8 py-3 w-fit">
-          {tagline}
+      <div className="px-5 pt-16">
+        {/* heading */}
+        <div className="flex justify-center items-center gap-5 flex-col text-center">
+          {/* tagline */}
+          <div className="capitalize rounded-3xl bg-red-200 text-red-800 font-medium px-8 py-3 w-fit">
+            {tagline}
+          </div>
+          {/* heading text */}
+          <h2 className="capitalize font-semibold text-2xl text-[#1a1a1a]">
+            {heading}
+          </h2>
         </div>
-        {/* heading text */}
-        <h2 className="capitalize font-semibold text-2xl text-[#1a1a1a]">
-          {heading}
-        </h2>
+
+        {/* image */}
+        <div className="rounded-md my-7 flex items-center justify-center">
+          <img src={imgUrl} alt="" className=" rounded-md" />
+        </div>
+
+        {/* Information */}
+        <h3 className="uppercase text-[#111] font-medium text-xl my-10">
+          {courseName}
+        </h3>
+
+        {/* parent */}
+        <div className="flex flex-col gap-10">
+          {/* about */}
+          <div className="">
+            <h4 className="font-medium   text-lg mb-1">About the department</h4>
+            <p className="font-light ">{aboutCourse}</p>
+          </div>
+
+          {/* Why */}
+          <div className="">
+            <h4 className="font-medium text-lg mb-1">Why recommended</h4>
+            <p className="font-light">{whyRecommended}</p>
+          </div>
+
+          {/* Programs */}
+          <div className="">
+            <h4 className="font-medium text-lg mb-1">Programs</h4>
+            <ul className="font-light ms-2 list-disc list-inside">
+              {programs.map((prog, index) => (
+                <li key={index}>{prog}</li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Programs */}
+          <div className="">
+            <h4 className="font-medium text-lg mb-1">Career path</h4>
+            <ul className="font-light ms-2 list-disc list-inside">
+              {careerPath.map((career, index) => (
+                <li key={index}>{career}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
 
-      {/* image */}
-      <div className="rounded-md my-7 flex items-center justify-center">
-        <img src={imgUrl} alt="" className=" rounded-md" />
-      </div>
-
-      {/* Information */}
-      <h3 className="uppercase text-[#111] font-medium text-xl my-10">
-        {courseName}
-      </h3>
-
-      {/* parent */}
-      <div className="flex flex-col gap-10">
-        {/* about */}
-        <div className="">
-          <h4 className="font-medium   text-lg mb-1">About the department</h4>
-          <p className="font-light ">{aboutCourse}</p>
-        </div>
-
-        {/* Why */}
-        <div className="">
-          <h4 className="font-medium text-lg mb-1">Why recommended</h4>
-          <p className="font-light">{whyRecommended}</p>
-        </div>
-
-        {/* Programs */}
-        <div className="">
-          <h4 className="font-medium text-lg mb-1">Programs</h4>
-          <ul className="font-light ms-2 list-disc list-inside">
-            {programs.map((prog, index) => (
-              <li key={index}>{prog}</li>
-            ))}
-          </ul>
-        </div>
-
-        {/* Programs */}
-        <div className="">
-          <h4 className="font-medium text-lg mb-1">Career path</h4>
-          <ul className="font-light ms-2 list-disc list-inside">
-            {careerPath.map((career, index) => (
-              <li key={index}>{career}</li>
-            ))}
-          </ul>
-        </div>
+      <div className="text-center mt-10 py-5 bg-blue-primary text-white rounded-b-lg  border-1 border-black/10">
+        <p className="font-semibold tracking-wide uppercase text-lg">
+          Model {model}
+        </p>
       </div>
     </div>
   );
