b250b44303423377a914b4c42b50dbe8acc0642f
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-10-09 20:02:21 +0800

    feat: frame animation.

--- client/src/components/CourseImageCard.jsx
+++ client/src/components/CourseImageCard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default function CourseImageCard({ img_url = "" }) {
+import { motion } from "framer-motion";
+
+export default function CourseImageCard({ img_url = "", variantItem }) {
   return (
-    <div className="h-[120px] md:h-[150px] xl:h-[200px] bg-white rounded-lg">
+    <motion.div
+      variants={variantItem}
+      className="h-[120px] md:h-[150px] xl:h-[200px] bg-white rounded-lg"
+    >
       <img src={img_url} alt="" className="w-full h-full object-contain p-5" />
-    </div>
+    </motion.div>
   );
 }
