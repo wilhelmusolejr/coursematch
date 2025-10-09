b2adddb2634ea4156b6cadb4d6bd8d286a8a5d01
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-10-09 20:02:07 +0800

    feat: framer animation

--- client/src/components/FeatureCard.jsx
+++ client/src/components/FeatureCard.jsx
@@ -1,13 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
+import { motion } from "framer-motion";
+
 export default function FeatureCard({
   heading = "",
   description = "",
   icon = "",
+  variantItem,
 }) {
   return (
-    <div className="bg-white w-75 md:w-68 rounded-xl p-5 py-7 shadow-lg flex flex-col border-1 border-black/30">
+    <motion.div
+      variants={variantItem}
+      className="bg-white w-75 md:w-68 rounded-xl p-5 py-7 shadow-lg flex flex-col border-1 border-black/30"
+    >
       {/* icon */}
       <div className="h-12 w-12 bg-blue-100 rounded-full mb-10 flex items-center justify-center">
         <FontAwesomeIcon icon={icon} className="text-blue-600 h-6 w-6 m-3" />
@@ -20,6 +26,6 @@ export default function FeatureCard({
 
       {/* paragraph */}
       <p className="text-black-tertiary font-light">{description}</p>
-    </div>
+    </motion.div>
   );
 }
