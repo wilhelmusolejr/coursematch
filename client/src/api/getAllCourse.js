18a53622f7aee5ff5be83503298c4672c38b536a
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-10-16 19:59:30 +0800

    refactor: modify hardcoded url link to .env backend_url

--- client/src/api/getAllCourse.js
+++ client/src/api/getAllCourse.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/courses/";
+const API_URL = import.meta.env.VITE_DJANGO_API_URL;
 
 export const getAllCourse = async (payload) => {
   try {
